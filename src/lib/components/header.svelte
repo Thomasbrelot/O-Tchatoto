0132ab7f3fba224024364b348c5d392dfcff1a9b
Thomas <brelotthomas@gmail.com>
2025-06-01 19:33:54 +0200

    💾 Sauvegarde avant pull

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,19 +1,20 @@
 <script>
-	// La fonction toggleDrawer est passée en prop par le parent (ex: +page.svelte)
-	export let toggleDrawer;
+  // La fonction toggleDrawer est passée en prop par le parent (ex: +page.svelte)
+  export let toggleDrawer;
 </script>
 
 <header class="app-header">
-	<button
-		class="burger-button"
-		type="button"
-		on:click={toggleDrawer}
-		aria-label="Ouvrir le menu de navigation"
-	>
-		<span class="bar"></span>
-		<span class="bar"></span>
-		<span class="bar"></span>
-	</button>
+  <button
+    class="burger-button"
+    type="button"
+    on:click={toggleDrawer}
+    aria-label="Ouvrir le menu de navigation"
+  >
+    <span class="bar"></span>
+    <span class="bar"></span>
+    <span class="bar"></span>
+  </button>
 
-	<h1 class="app-title">O'Tchatoto</h1>
+  <img src="/image/Logo-O'Tchatoto.png" alt="Représentation du logo du tchat 'O'Tchatoto'" />
+  <h1 class="app-title">O'Tchatoto</h1>
 </header>
